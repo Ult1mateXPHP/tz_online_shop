7187c47a4e4e43729c1724e63a71e9da5a8bf639
Ult1mateXPHP <alex.ultimatex@gmail.com>
2024-09-26 17:16:44 +0300

    update

---
+++ src/Application/Overview/ProductApi.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace OnlineShop\Application\Overview;
+
+class ProductApi
+{
+
+}
---
+++ src/Domain/Entity/ProductEntity.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OnlineShop\Domain\Entity;
+
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    //use HasFactory;
+
+    protected $table = 'product';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'count'
+    ];
+}
